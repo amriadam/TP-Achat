82a121590a7a817ad8263b923b7b1aa9b04b0b76
amriadam <adam.elamri@esprit.tn>
2022-11-04 17:34:39 +0100

    Adam mokito and junit tests for the Facture services

--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -1,48 +1,108 @@
 package com.esprit.examen.services;
 
+import com.esprit.examen.entities.CategorieFournisseur;
 import com.esprit.examen.entities.Facture;
+import com.esprit.examen.entities.Fournisseur;
 import com.esprit.examen.repositories.FactureRepository;
+import com.esprit.examen.repositories.FournisseurRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-import javax.persistence.EntityNotFoundException;
-import java.time.LocalTime; // import the LocalTime class
 import java.sql.Date;
+import java.util.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class FactureServiceImplTest {
 
+/*----------------------------------------------------------------------------------------------------*/
     @Autowired
-    private  FactureServiceImpl serviceFacture;
-
+    FactureRepository fr;
     @Autowired
-    private FactureRepository factureRepository;
+    FactureServiceImpl fs;
+    @InjectMocks
+    FactureServiceImpl factureService;
+    @Mock
+    FactureRepository dao;
+    @Mock
+    FournisseurRepository daoFournisseur;
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    /*----------------------------------------------------------------------------------------------------*/
 
     @Test
-    public void addFacture() {
-        Facture f = new Facture();
-        f.setIdFacture(1L);
-        f.setMontantFacture(100);
-        f.setArchivee(false);
-        f.setDetailsFacture(null);
-        f.setReglements(null);
-        f.setDateCreationFacture(new Date(2000, 11, 21));
-        f.setMontantRemise(10);
-        f.setDateDerniereModificationFacture(new Date(2010, 11, 21));
-        f.setFournisseur(null);
-
-        serviceFacture.addFacture(f);
-        assertNotNull(factureRepository.findById(1L).get());
+    public void addFactureTest() {
+        Facture f = new Facture(1L,10,100,new Date(2000, 10, 21),new Date(2022, 11, 23),true,null,null,null);
+        factureService.addFacture(f);
+        verify(dao,times(1)).save(f);
     }
 
+    /*----------------------------------------------------------------------------------------------------*/
+
+
+
+    @Test
+    public  void retrieveAllFacturesTest(){
+        List <Facture> factureList = new ArrayList<Facture>();
+        Facture facture1 = new Facture(1L,10,100,new Date(2000, 10, 21),new Date(2022, 11, 23),true,null,null,null);
+        Facture facture2 = new Facture(2L,20,150,new Date(2022, 11, 1),new Date(2022, 10, 23),false,null,null,null);
+        factureList.add(facture1);
+        factureList.add(facture2);
+        when(dao.findAll()).thenReturn(factureList);
+        //test
+        List<Facture> empList = factureService.retrieveAllFactures();
+        assertEquals(2, empList.size());
+        verify(dao, times(1)).findAll();
+
+    }
+    /*----------------------------------------------------------------------------------------------------*/
+
+    private static final double DELTA = 1e-15;
+
+    @Test
+    public void retrieveFactureByIdTest(){
+        Facture f = new Facture(1L,10,100,new Date(2000, 10, 21),new Date(2000, 10, 21),true,null,null,null);
+        when(dao.findById(1L)).thenReturn(Optional.of(f));
+        Facture fact = factureService.retrieveFacture(1L);
+        assertEquals(10,fact.getMontantRemise(),DELTA);
+        assertEquals(100,fact.getMontantFacture(),DELTA);
+        assertEquals(new Date(2000, 10, 21),fact.getDateCreationFacture());
+    }
+    /*----------------------------------------------------------------------------------------------------*/
+
+    @Test
+    public void retrieveFactureByFournisseurTest(){
+        Facture f = new Facture(1L,10,100,new Date(2000, 10, 21),new Date(2000, 10, 21),true,null,null,null);
+        Set<Facture> setFacture = new HashSet<>();
+        setFacture.add(f);
+        Fournisseur fournisseur = new Fournisseur(1L,"f1","lib1", CategorieFournisseur.ORDINAIRE,setFacture,null,null);
+        when(daoFournisseur.save(fournisseur)).thenReturn(fournisseur);
+        assertNotNull(fournisseur.getFactures());
+    }
+    /*----------------------------------------------------------------------------------------------------*/
+
+    @Test
+    public void cancelFactureTest(){
+        Facture f = new Facture(6L,10,100,new Date(2000, 10, 21),new Date(2000, 10, 21),false,null,null,null);
+        fs.addFacture(f);
+        fs.cancelFacture(6L);
+        Facture fres = fr.findById(6L).get();
+        assertEquals(true,fres.getArchivee());
+    }
 
 
 
